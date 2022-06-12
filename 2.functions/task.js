13cf36fe91b4c2a356a3aab2139e55b5868a034a
RomanEvd <evdokimov.roman.1993@mail.ru>
2022-06-13 00:20:31 +0400

    Function second edit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,32 +4,31 @@ function getArrayParams(arr) {
   let max = -Infinity;
   let sum = 0;
   let avg = 0;
-  for (let i = 0; i < arr.lenght; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) { 
-      max = arr[i]
+      max = arr[i];
     }
     if (arr[i] < min) {
-      min = arr[i]
+      min = arr[i];
     }
     sum = sum + arr[i];
   }
-  avg = average(sum / arr.lenght).toFixed(2);
+  avg = parseFloat(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum = 0;
-  for (let i = 0; i< arr.lenght; i++) {
+  for (let i = 0; i< arr.length; i++) {
     sum = sum + arr[i];
   }
-
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
-  for (let i = 0; i < arrOfArr.lenght; i++) {
+  for (let i = 0; i < arrOfArr.length; i++) {
     let arrSum = func(arrOfArr[i]);
     if (arrSum > max) {
       max = arrSum;
@@ -40,6 +39,6 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-  return (getArrayParams.max(arr), getArrayParams.min(arr)) => Math.abs(getArrayParams.max(arr) - getArrayParams.min(arr));
-  
+ let difference = Math.abs(getArrayParams(arr).max - getArrayParams(arr).min);  
+return difference;
 }
