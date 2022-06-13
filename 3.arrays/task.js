8eead0914bd63c5077188dde487a4e996ad0a493
RomanEvd <evdokimov.roman.1993@mail.ru>
2022-06-13 22:18:47 +0400

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
+ let result
 
-  // Ваш код
-
-  return result; // boolean
+  return (arr1.length === arr2.length) && (arr1.every((val,idx) => val === arr2[idx]));
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  return arr.filter(val => val > 0).filter(val => val % 3 === 0).map(val => val * 10);
 
-  return resultArr; // array
 }
