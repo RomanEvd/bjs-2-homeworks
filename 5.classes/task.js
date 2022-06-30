e6ae7993a9cf6de82c104e58bbed9dd17c32e630
RomanEvd <evdokimov.roman.1993@mail.ru>
2022-06-30 16:53:22 +0400

    classes

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,99 +1,98 @@
-// Задача №1.
-
+//Задача №1. 
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
+    constructor (name, releaseDate, pagesCount) {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        state = 100;
-        type = null;
+        this.state = 100;
+        this.type = null;
     }
+
     fix() {
-        return this.state *= 1.5;
+        return this.state *=1.5;
     }
+
     set state(state) {
-        if(state < 0) {
-            this.state = 0;
+        if (state > 100) {
+            this._state = 100;
         }
-        else if(state > 100) {
-            this.state = 100;
+        else if (state < 0) {
+            this._state = 0;
         }
         else {
-            this.state = state;
-        }
+            this._state = state;
+        }  
     }
+
     get state() {
-        return this.state;
+        return this._state;
     }
 }
 
 class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.type = "magazine";
+    constructor (name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
+        this.type = 'magazine';
     }
 }
 
 class Book extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount, author) {
-        super(name, releaseDate, pagesCount);
+    constructor (author, name, releaseDate, pagesCount) {
+        super (name, releaseDate, pagesCount);
         this.author = author;
-        this.type = "book";   
+        this.type = 'book';
     }
 }
 
 class NovelBook extends Book {
-    constructor(name, releaseDate, pagesCount, author) {
-        super(name, releaseDate, pagesCount, author); 
-        this.type = "novel";
-    }
-}
-
-class FantasticBook extends Book {
-    constructor(name, releaseDate, pagesCount, author) {
-        super(name, releaseDate, pagesCount, author); 
-        this.type = "fantastic";
+    constructor (author, name, releaseDate, pagesCount) {
+        super (author, name, releaseDate, pagesCount);
+        this.type = 'novel';
     }
 }
 
 class DetectiveBook extends Book {
-    constructor(name, releaseDate, pagesCount, author) {
-        super(name, releaseDate, pagesCount, author); 
-        this.type = "detective";
+    constructor (author, name, releaseDate, pagesCount) {
+        super (author, name, releaseDate, pagesCount);
+        this.type = 'detective';
     }
 }
 
+class FantasticBook extends Book {
+    constructor (author, name, releaseDate, pagesCount) {
+        super (author, name, releaseDate, pagesCount);
+        this.type = 'fantastic';
+    }
+}
 
-// Задача №2.
-
+//Задача №2. 
 class Library {
-    constructor(name) {
+    constructor (name) {
         this.name = name;
         this.books = [];
     }
-}
 
-addBook(book); {
-    if(book.state > 30) {
-        return this.books.push(book);
+    addBook(book) {
+        if (book.state > 30) {
+            return this.books.push(book);
+        }
+        return;
     }
-}
 
-findBookBy(type, value); {
-    let searchResult = this.books.find(book => book[type] === value);
-    return (typeof searchResult === "object") ? searchResult : null;
-}
-
-giveBookByName(bookName); {
-    let reqBook  = this.books.find(book => book.name === bookName);
-    if(typeof reqBook === "object") {
-        this.books.splice(this.books.indexOf(reqBook), 1);
-        return reqBook;
+    findBookBy(type, value) {
+        let searchResult = this.books.find(book => book[type] === value);
+        return (typeof searchResult === 'object') ? searchResult : null; 
     }
-    else {
-        return null;
+        
+    giveBookByName(bookName) {
+       let reqBook = this.books.find(book => book.name === bookName);
+       if (typeof reqBook === 'object') {
+            this.books.splice(this.books.indexOf(reqBook), 1);
+            return reqBook;
+       }
+       else return null;
     }
+     
 }
 
-
-//Задача №3.
+//Задача №3. 
